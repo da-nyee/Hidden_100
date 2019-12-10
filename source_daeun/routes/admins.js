03b63da2faaf470f40dc2d3c6fb753414b028119
daeun <leede0418@likelion.org>
2019-12-10 21:43:51 +0900

    feat: Modify Sign up, Sign in, Sign out (admin)

--- source_daeun/routes/admins.js
+++ source_daeun/routes/admins.js
@@ -26,32 +26,41 @@ const PrintSignup = (req, res) => {
     htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
 
     res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
-    res.end(ejs.render(htmlstream));
+   if(req.session.auth==undefined){req.session.auth=0;}
+   if(req.session.who==undefined){req.session.who='0';}
+
+    res.end(ejs.render(htmlstream, {
+    auth:req.session.auth ,
+    mem_id:req.session.who,
+    }));
 };
 
 const HandleSignup = (req, res) => {
     let body = req.body;
     let htmlstream = '';
 
-    if(body.id == '' || body.pass1 == '' || body.name == '' || body.addr == ''){
-        console.log("데이터 입력이 되지 않아 DB에 저장할 수 없습니다.");
-        res.status(561).end('<meta charset="utf-8">데이터가 입력되지 않아 관리자가입을 할 수 없습니다!');
+    let admin_pass1, admin_pass2;
+
+    if(body.pass1 != body.pass2){
+        console.log("비밀번호가 서로 다릅니다. 다시 확인해주세요!");
+        res.send('<script type="text/javascript">alert("비밀번호가 서로 다릅니다. 다시 확인해주세요!"); location.href="/admins/reg"; </script>');
     }
     else{
-        db.query('INSERT INTO t1_admin(admin_id, admin_pass, admin_name) VALUES(?,?,?)',
-        [body.id, body.pass1, body.name, body.addr], (error, results, fields) => {
+
+        db.query('INSERT INTO t1_admin(admin_name, admin_id, admin_pass, admin_email, admin_phone, admin_bday) VALUES(?,?,?,?,?,?)',
+        [body.name, body.id, body.pass1, body.email, body.phone, body.bday], (error, results, fields) => {
             if(error){
                 htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
                 res.status(562).end(ejs.render(htmlstream, {
                     'title':'Hidden 100',
-                    'warn_title':'관리자가입 오류',
+                    'warn_title':'관리자 가입 오류',
                     'warn_message':'이미 관리자로 등록되어 있습니다!',
                     'return_url':'/admins/auth'
                 }));
             }
             else{
-                console.log("관리자가입에 성공하셨습니다! 신규 관리자로 등록되었습니다.");
-                res.redirect('/admins/auth');
+                console.log("관리자 가입에 성공하셨습니다! 신규 관리자로 등록되었습니다.");
+                res.send('<script type="text/javascript">alert("관리자 가입에 성공하셨습니다! 신규 관리자로 등록되었습니다."); location.href="/admins/auth"; </script>');
             }
         });
     }
@@ -74,20 +83,18 @@ const PrintSignin = (req, res) => {
 
     if(req.session.auth){ // true: 로그인 상태, false: 비로그인 상태
         res.end(ejs.render(htmlstream, {
-            'title':'Hidden 100',
-            'regurl':'/admins/profile',
-            'reglabel':req.session.who,
-            'logurl':'/admins/logout',
-            'loglabel':'로그아웃'
+ 
+        auth:0 ,
+        mem_id:0
+
         }));
     }
     else{
         res.end(ejs.render(htmlstream, {
-            'title':'Hidden 100',
-            'regurl':'/admins/reg',
-            'reglabel':'회원가입',
-            'logurl':'/admins/auth',
-            'loglabel':'로그인'
+ 
+        auth:0 ,
+        mem_id:0
+
         }));
     }
 };
@@ -101,49 +108,43 @@ const HandleSignin = (req, res) => {
     console.log(body.id);
     console.log(body.pass);
 
-    if(body.id == '' || body.pass == ''){
-        console.log("아이디 혹은 비밀번호가 입력되지 않아 로그인 할 수 없습니다!");
-        res.status(562).end('<meta charset="utf-8">아이디 혹은 비밀번호가 입력되지 않아 로그인 할 수 없습니다!');
-    }
-    else{
-        sql_str = "SELECT * from t1_admin where admin_id='"+body.id+"' and admin_pass='"+body.pass+"';";
-        console.log("SQL: " + sql_str);
+    sql_str = "SELECT * from t1_admin where admin_id='"+body.id+"' and admin_pass='"+body.pass+"';";
+    console.log("SQL: " + sql_str);
 
-        db.query(sql_str, (error, results, fields) => {
-            if(error){
-                res.status(562).end("Login fails as there is no id in DB!");
+    db.query(sql_str, (error, results, fields) => {
+        if(error){
+            res.status(562).end("Login fails as there is no id in DB!");
+        }
+        else{
+            if(results.length <= 0){ // select 조회 결과가 없는 경우
+                htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
+                res.status(562).end(ejs.render(htmlstream, {
+                    'title':'Hidden 100',
+                    'warn_title':'로그인 오류',
+                    'warn_message':'등록된 계정이나 비밀번호가 틀립니다.',
+                    'return_url':'/admins/reg'
+                }));
             }
-            else{
-                if(results.length <= 0){ // select 조회 결과가 없는 경우
-                    htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
-                    res.status(562).end(ejs.render(htmlstream, {
-                        'title':'Hidden 100',
-                        'warn_title':'로그인 오류',
-                        'warn_message':'등록된 계정이나 비밀번호가 틀립니다.',
-                        'return_url':'/admins/profile'
-                    }));
-                }
-                else{ // select 조회 결과가 있는 경우
-                    results.forEach((item, index) => {
-                        admin_id = item.admin_id;
-                        admin_pass = item.admin_pass;
-                        admin_name = item.admin_name;
-                        console.log("DB에서 로그인 성공한 ID/비밀번호: %s/%s", admin_id, admin_pass);
+            else{ // select 조회 결과가 있는 경우
+                results.forEach((item, index) => {
+                    admin_id = item.admin_id;
+                    admin_pass = item.admin_pass;
+                    admin_name = item.admin_name;
+                    console.log("DB에서 로그인 성공한 ID/비밀번호: %s/%s", admin_id, admin_pass);
 
-                        if(body.id == admin_id && body.pass == admin_pass){
-                            req.session.auth = 99; // 임의의 수로 로그인 성공 설정
-                            req.session.who = admin_name;
+                    if(body.id == admin_id && body.pass == admin_pass){
+                        req.session.auth = 99; // 임의의 수로 로그인 성공 설정
+                        req.session.who = mem_id;
 
-                            if(body.id == 'admin'){ // 인증된 사용자가 관리자(admin)인 경우 이를 표시
-                                req.session.admin = true;
-                            }
-                            res.redirect('/admins/profile');
+                        if(body.id == 'admin'){ // 인증된 사용자가 관리자(admin)인 경우 이를 표시
+                            req.session.admin = true;
                         }
-                    });
-                }
+                        res.send('<script type="text/javascript">alert("로그인 되었습니다!"); location.href="/"; </script>');
+                    }
+                });
             }
-        });
-    }
+        }
+    });
 }
 
 /* REST API의 URI와 handler를 mapping */
@@ -159,136 +160,6 @@ const HandleSignout = (req, res) => {
 /* REST API의 URI와 handler를 mapping */
 router.get('/logout', HandleSignout);
 
-/* 정보수정 */
-const PrintProfile = (req, res) => {
-    let htmlstream = '';
 
-    let body = req.body;
-    let user;
-
-    htmlstream = fs.readFileSync(__dirname + '/../views/header.ejs','utf8');
-    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_nav.ejs','utf8');
-    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_settings.ejs','utf8');
-    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
-
-    res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
-    res.end(ejs.render(htmlstream));
-};
-
-const HandleProfile = (req, res) => {
-    let body = req.body;
-    let htmlstream = '';
-
-    if(body.pass1 == '' || body.addr == ''){
-        console.log("데이터 입력이 되지 않아 DB에 저장할 수 없습니다.");
-        res.status(561).end('<meta charset="utf-8">데이터가 입력되지 않아 정보수정을 할 수 없습니다!');
-    }
-    else{
-        db.query("SELECT * from t1_admin where admin_id=?", [body.id], (error, results, fields) => {
-            if(!error){
-                db.query("UPDATE t1_admin SET admin_pass=? where admin_id=?", [body.pass1, body.id], (error, results, fields) => {
-                    if(error){
-                        htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
-                        res.status(562).end(ejs.render(htmlstream, {
-                            'title':'Hidden 100',
-                            'warn_title':'정보수정 오류',
-                            'warn_message':'정보수정에 실패하였습니다!',
-                            'return_url':'/admins/profile'
-                        }));
-                    }
-                    else{
-                        console.log("정보수정이 성공적으로 완료되었습니다!");
-                        res.redirect('/admins/profile');
-                    }
-                });
-            }
-        });
-    }
-};
-
-/* REST API의 URI와 handler를 mapping */
-router.get('/profile', PrintProfile);
-router.post('/profile', HandleProfile);
-
-/* 관리자탈퇴 */
-const PrintDeletion = (req, res) => {
-    let htmlstream = '';
-
-    htmlstream = fs.readFileSync(__dirname + '/../views/header.ejs','utf8');
-    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_nav.ejs','utf8');
-    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_deletion.ejs','utf8');
-    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
-    
-    res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
-
-    if(req.session.auth){ // true: 로그인 상태, false: 비로그인 상태
-        res.end(ejs.render(htmlstream, {
-            'title':'Hidden 100',
-            'regurl':'/admins/profile',
-            'reglabel':req.session.who,
-            'logurl':'/admins/logout',
-            'loglabel':'로그아웃'
-        }));
-    }
-    else{
-        res.end(ejs.render(htmlstream, {
-            'title':'Hidden 100',
-            'regurl':'/admins/reg',
-            'reglabel':'회원가입',
-            'logurl':'/admins/auth',
-            'loglabel':'로그인'
-        }));
-    }
-};
-
-const HandleDeletion = (req, res) => {
-    let body = req.body;
-    let admin_id, admin_pass;
-    let sql_str;
-    let htmlstream = '';
-
-    console.log(body.id);
-    console.log(body.pass);
-
-    if(body.id == '' || body.pass == ''){
-        console.log("아이디 혹은 비밀번호가 입력되지 않아 관리자탈퇴가 불가합니다!");
-        res.status(562).end('<meta charset="utf-8">아이디 혹은 비밀번호가 입력되지 않아 관리자탈퇴가 불가합니다!');
-    }
-    else{
-        sql_str = "SELECT * from t1_admin where admin_id='"+body.id+"' and admin_pass='"+body.pass+"';";
-        console.log("SQL: " + sql_str);
-
-        db.query(sql_str, (error, results, fields) => {
-            if(error){
-                res.status(562).end("Login fails as there is no id in DB!");
-            }
-            else{
-                db.query("SELECT * from t1_admin where admin_id=?", [body.id], (error, results, fields) => {
-                    if(!error){
-                        db.query("DELETE from t1_admin where admin_id=? and admin_pass=?", [body.id, body.pass], (error, results, fields) => {
-                            if(error){
-                                htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
-                                res.status(562).end(ejs.render(htmlstream, {
-                                    'title':'Hidden 100',
-                                    'warn_title':'관리자탈퇴 오류',
-                                    'warn_message':'관리자탈퇴에 실패하였습니다!',
-                                    'return_url':'/admins/reg'
-                                }));
-                            }
-                            else{
-                                console.log("관리자탈퇴 되었습니다.");
-                                res.redirect('/admins/reg');
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    }
-}
-
-/* REST API의 URI와 handler를 mapping */
-router.get('/deletion', PrintDeletion);
-router.post('/deletion', HandleDeletion);
 
 module.exports = router;
