2c79a22d9bf7c3a9af54b267acb1e2dd41ad026d
daeun <leede0418@likelion.org>
2019-12-13 00:05:25 +0900

    feat: Add a keyword search box

--- source_daeun/routes/user_manage.js
+++ source_daeun/routes/user_manage.js
@@ -293,4 +293,49 @@ const HandleControl_setting = (req, res) => {
 router.get('/control_setting', PrintControl_setting);
 router.post('/control_setting', HandleControl_setting);
 
+/* 신고회원 조회 */
+const getReported = (req, res) => {
+    if(req.session.auth==91){
+ 
+        let htmlstream = '';
+
+        htmlstream = fs.readFileSync(__dirname + '/../views/admin_header.ejs','utf8');
+        htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_nav.ejs','utf8');
+        htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_reported.ejs','utf8');
+        htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
+
+        const sql_settings = "SELECT * from t1_report";
+
+        db.query(sql_settings, (error, results, fields) => {
+            res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
+
+            if(req.session.auth){
+                res.end(ejs.render(htmlstream, {
+                    reports:results,
+                    auth:req.session.auth,
+                    admin_id:req.session.who
+                }));
+            }
+            else{
+                res.end(ejs.render(htmlstream, {
+                    auth:req.session.auth,
+                    admin_id:req.session.who
+                }))
+            }
+        });}
+    else{
+        htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
+
+        res.status(562).end(ejs.render(htmlstream, {
+            'title':'Hidden 100',
+            'warn_title':'로그인 오류',
+            'warn_message':'로그인이 필요한 서비스입니다.',
+            'return_url':'/admin/admins/auth'
+        }));
+    }
+};
+
+/* REST API의 URI와 handler를 mapping */
+router.get('/getReported', getReported);
+
 module.exports = router;
