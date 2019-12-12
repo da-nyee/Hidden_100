847be07bcba7403c64e834b39de0e4f2461a21b9
qkdrnvhrrur <amicusadaras6@gmail.com>
2019-12-12 20:34:08 +0900

    신고 게시판

--- dev/v0.7/routes/user_manage.js
+++ dev/v0.7/routes/user_manage.js
@@ -216,4 +216,49 @@ const HandleControl_status = (req, res) => {
 router.get('/control_status', PrintControl_status);
 router.post('/control_status', HandleControl_status);
 
+/*신고회원 조회*/
+const getRepoted=(req, res)=>{
+    if(req.session.auth==91){
+ 
+        let htmlstream = '';
+
+        htmlstream = fs.readFileSync(__dirname + '/../views/admin_header.ejs','utf8');
+        htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_nav.ejs','utf8');
+        htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/admin_reported.ejs','utf8');
+        htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
+
+        const sql_settings = "SELECT * from t1_report";
+
+        db.query(sql_settings, (error, results, fields) => {
+            res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
+            console.log(results);
+
+            if(req.session.auth){
+                res.end(ejs.render(htmlstream, {
+                    reports:results,
+                    auth:req.session.auth,
+                    admin_id:req.session.who
+                }));
+            }
+            else{
+                res.end(ejs.render(htmlstream, {
+                    auth:req.session.auth,
+                    admin_id:req.session.who
+                }))
+            }
+        });}
+    else{
+        htmlstream = fs.readFileSync(__dirname + '/../views/alert.ejs','utf8');
+
+        res.status(562).end(ejs.render(htmlstream, {
+            'title':'Hidden 100',
+            'warn_title':'로그인 오류',
+            'warn_message':'로그인이 필요한 서비스입니다.',
+            'return_url':'/admin/admins/auth'
+        }));
+    }
+};
+
+router.get('/getReported', getRepoted);
+
 module.exports = router;
